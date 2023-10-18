fbc0e832c110196a9209d6b0b75e0776c4550da4
Mag Millen-Dutka <145022728+MagMillen-Dutka@users.noreply.github.com>
2023-10-18 19:02:22 +0100

    updated the JS file to correct bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,56 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Variables
-    var passwordText = "";
-    var password = "";
-    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lowercase = "asbcdefghijklmnopqrstuvwxyz";
-    var numbers = "0123456789";
-    var symbols = "!£$%^&*#";
-    var keyLength = "";
-    var multiSelect = "";
-    var randomKey = "";
-    var upperABC = "";
-    var lowerABC = "";
-    var numb = "";
-    var symb = "";
-
+// A function to get the password
     function getPassword() {
-    keyLength = prompt("How long would you like the password to be?", "Enter a number between 8 to 128.");
-          
+    
+// Password variables for allowable passwords characters
+    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var lower = "abcdefghijklmnopqrstuvwxyz"
+    var numbers = "0123456789"
+    var signs = "!£$%^&*#"
+    var characters = "";
+    var randomKey = "";
+
+// First prompt to explain criteria
+    var keyLength = prompt(
+        "Your password must be between 8 to 128 characters long."
+    );
+    
+// If user selects wrong criteria, this section will explain they need to try again.
     if (keyLength < 8 || keyLength > 128 || isNaN(keyLength)) {
-      prompt("The password must be between 8 and 128 characters in length. Enter another number.");
-    return;
+        alert("The password must be between 8 and 128 characters in length. Try Again.");        
+        return;
 
-  } else {
-  upperABC = confirm("The password will contain uppercase letters.");
-  lowerABC = confirm("The password will contain lowercase letters.");
-  numb = confirm("The password will contain numbers.");
-  symb = confirm("The password will contain symbols.");
-  }
+// Confirmation to user of values to be used in password.
+    } else {
+    var uppercase = confirm("Your password will contain a variety of uppercase letters, click ok to continue");
+    if (uppercase) {characters += upper;}
+    var lowercase = confirm("Your password will contain a variety of lowercase letters, click ok to continue");
+    if (lowercase) {characters += lower;}
+    var symbols = confirm("Your password will contain a variety of symbols, click ok to continue");
+    if (symbols) {characters += signs;}
+    var digits = confirm("Your password will contain numbers, click ok to continue");
+    if (digits) {characters += numbers;}
+    
+    }
 
-  if (upperABC) {multiSelect += uppercase;}
-  if (lowerABC) {multiSelect += lowercase;}
-  if (numb) {multiSelect += numbers;}
-  if (symb) {multiSelect += symbols;}
- 
-  for (i = 0; i < keyLength; i++) {
+    for (i = 0; i < keyLength; i++) {
 
-      randomKey += multiSelect[Math.floor(Math.random() * multiSelect.length)]
-  }
-  return(randomKey);
+// Making the password from the criteria above
+        randomKey += characters[Math.floor(Math.random() * characters.length)]
+    }
+    return(randomKey);
 }
 
-// Write password to the #password input. The text box area where the newly generated password will appear.
-  passwordText = document.querySelector("#password");
-  function writePassword(randomKey) {
-  if (password.length === 0) {
-      return;
-  }
-  passwordText.valueOf = randomKey;
+// Displaying password from the criteria above
+    var passwordText = document.querySelector("#password");
+    function writePassword(randomKey) {
+    if (password.length === 0) {
+        return;
+    }
+    passwordText.value = randomKey;
 }
 
-  generateBtn.addEventListener("click", function () { writePassword(getPassword()) });
 
-  
+    
